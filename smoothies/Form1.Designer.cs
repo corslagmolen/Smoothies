44e5594d1542aa24a22728db2e1b3eccba214b17
Cor Slagmolen <cor.slagmolen@gmail.com>
2019-08-22 18:43:58 +0200

    Formopmaak V0.1

--- smoothies/Form1.Designer.cs
+++ smoothies/Form1.Designer.cs
@@ -28,13 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnZoek = new System.Windows.Forms.Button();
+            this.btnWisSelectie = new System.Windows.Forms.Button();
+            this.btnWisIngredient = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblIngredient = new System.Windows.Forms.Label();
+            this.txbIngredient = new System.Windows.Forms.TextBox();
+            this.livIngredienten = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnZoek
+            // 
+            this.btnZoek.Location = new System.Drawing.Point(12, 54);
+            this.btnZoek.Name = "btnZoek";
+            this.btnZoek.Size = new System.Drawing.Size(113, 20);
+            this.btnZoek.TabIndex = 0;
+            this.btnZoek.Text = "zoek in lijst";
+            this.btnZoek.UseVisualStyleBackColor = true;
+            // 
+            // btnWisSelectie
+            // 
+            this.btnWisSelectie.Location = new System.Drawing.Point(12, 90);
+            this.btnWisSelectie.Name = "btnWisSelectie";
+            this.btnWisSelectie.Size = new System.Drawing.Size(113, 65);
+            this.btnWisSelectie.TabIndex = 1;
+            this.btnWisSelectie.Text = "Maak Selectie ongedaan";
+            this.btnWisSelectie.UseVisualStyleBackColor = true;
+            // 
+            // btnWisIngredient
+            // 
+            this.btnWisIngredient.Location = new System.Drawing.Point(12, 173);
+            this.btnWisIngredient.Name = "btnWisIngredient";
+            this.btnWisIngredient.Size = new System.Drawing.Size(113, 65);
+            this.btnWisIngredient.TabIndex = 2;
+            this.btnWisIngredient.Text = "Verwijder ingredient";
+            this.btnWisIngredient.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 263);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(113, 20);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Einde";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // lblIngredient
+            // 
+            this.lblIngredient.AutoSize = true;
+            this.lblIngredient.Location = new System.Drawing.Point(71, 25);
+            this.lblIngredient.Name = "lblIngredient";
+            this.lblIngredient.Size = new System.Drawing.Size(54, 13);
+            this.lblIngredient.TabIndex = 4;
+            this.lblIngredient.Text = "Ingredient";
+            // 
+            // txbIngredient
+            // 
+            this.txbIngredient.Location = new System.Drawing.Point(153, 25);
+            this.txbIngredient.Name = "txbIngredient";
+            this.txbIngredient.Size = new System.Drawing.Size(193, 20);
+            this.txbIngredient.TabIndex = 5;
+            // 
+            // livIngredienten
+            // 
+            this.livIngredienten.Location = new System.Drawing.Point(153, 54);
+            this.livIngredienten.Name = "livIngredienten";
+            this.livIngredienten.Size = new System.Drawing.Size(193, 229);
+            this.livIngredienten.TabIndex = 6;
+            this.livIngredienten.UseCompatibleStateImageBehavior = false;
+            this.livIngredienten.DrawColumnHeader += new System.Windows.Forms.DrawListViewColumnHeaderEventHandler(this.LivIngredienten_DrawColumnHeader);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.livIngredienten);
+            this.Controls.Add(this.txbIngredient);
+            this.Controls.Add(this.lblIngredient);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnWisIngredient);
+            this.Controls.Add(this.btnWisSelectie);
+            this.Controls.Add(this.btnZoek);
+            this.Name = "Form1";
+            this.Text = "Smoothies";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnZoek;
+        private System.Windows.Forms.Button btnWisSelectie;
+        private System.Windows.Forms.Button btnWisIngredient;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblIngredient;
+        private System.Windows.Forms.TextBox txbIngredient;
+        private System.Windows.Forms.ListView livIngredienten;
     }
 }
 
--- smoothies/Form1.cs
+++ smoothies/Form1.cs
@@ -12,9 +12,47 @@ namespace smoothies
 {
     public partial class Form1 : Form
     {
+        string[] ingredienten = { "Appel", "Peer", "Aardbei", "Komkommer", "Yoghurt", "Meloen" };
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            opmaakLivIngredienten();
+            filLivIngredienten();
+        }
+        private void filLivIngredienten()
+        {
+            livIngredienten.Items.Clear();
+            foreach (var ingredient in ingredienten)
+            {
+                ListViewItem livIngredientenItem = new ListViewItem(ingredient);
+                livIngredienten.Items.Add(livIngredientenItem);
+            }
+        }
+        private void opmaakLivIngredienten()
+        {
+            livIngredienten.View = View.Details;
+            livIngredienten.OwnerDraw = true;
+            ColumnHeader headerIngredienten = livIngredienten.Columns.Add("Ingredienten", -2);
+
+
+        }
+
+        private void LivIngredienten_DrawColumnHeader(object sender, DrawListViewColumnHeaderEventArgs e)
+        {
+            StringFormat stringFormat = new StringFormat();
+            stringFormat.Alignment = StringAlignment.Center;
+            Font headerFont = new Font("Microsoft sans Serif", 10, FontStyle.Bold);
+            e.Graphics.FillRectangle(Brushes.Turquoise, e.Bounds);
+            e.Graphics.DrawString(e.Header.Text, headerFont, Brushes.Black, e.Bounds, stringFormat);
         }
     }
 }
